995e68e3118f297c0ab58cab458aefeb92c6afd4
Han-MZ <75778526+Han-MZ@users.noreply.github.com>
2024-05-15 15:16:51 +0800

    Update constants.py

--- src/constants.py
+++ src/constants.py
@@ -1,12 +1,10 @@
 # Global constants
 
 # Dataset locations
-IMDB_SVCD = "~/data/HR/ChangeDetectionDataset/"
-IMDB_LEVIRCD = "/home/xutao/hanmz/datas/levircd_512/"
-IMDB_WHU = "/home/hanmz/workspace/data/WHU-CD-256-CDD/"
-IMDB_NALand = "/home/hanmz/workspace/data/NALand_CDD"
-IMDB_S2Looking = "/home/xutao/hanmz/datas/s2looking_512/"
-IMDB_CDD = "/home/xutao/hanmz/datas/s2looking_512/"
+
+IMDB_LEVIRCD = "~/data/levircd_512/"
+IMDB_S2Looking = "~/datas/s2looking_512/"
+IMDB_CDD = "~/datas/cdd_512/"
 # Template strings
 CKP_LATEST = "checkpoint_latest.pth"
 CKP_BEST = "model_best.pth"
